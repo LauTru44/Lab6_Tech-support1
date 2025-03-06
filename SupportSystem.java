5178e2810c8d415fdcf4a76ce60c28b49a01cf97
laura <lauravtd44@gmail.com>
2025-03-06 09:19:23 -0500

    Initial sharing of project

---
+++ SupportSystem.java
@@ -0,0 +1,84 @@
+/**
+ * This class implements a technical support system. It is the top level class 
+ * in this project. The support system communicates via text input/output 
+ * in the text terminal.
+ * 
+ * This class uses an object of class InputReader to read input from the user,
+ * and an object of class Responder to generate responses. It contains a loop
+ * that repeatedly reads input and generates output until the users wants to 
+ * leave.
+ * 
+ * @author     Michael Kölling and David J. Barnes
+ * @version    0.1 (2016.02.29)
+ */
+/*
+ * 2) Summaries about the implementation of constuctors, methods and fields.
+ * 3) one evaluates if the beggining of a string is equivalent to a given prefix
+ *    the other one evaluates the substring in the same way.  
+ * 4) endsWith()
+ * 5) Length()
+ * 6) It is easy because it was in the "methods" section and in alphabetical order.
+ * 7) String trim()
+ *
+ */
+public class SupportSystem
+{
+    private InputReader reader;
+    private Responder responder;
+    
+    /**
+     * Creates a technical support system.
+     */
+    public SupportSystem()
+    {
+        reader = new InputReader();
+        responder = new Responder();
+    }
+
+    /**
+     * Start the technical support system. This will print a welcome
+     * message and enter into a dialog with the user, until the user
+     * ends the dialog.
+     */
+    public void start()
+    {
+        boolean finished = false;
+
+        printWelcome();
+
+        while(!finished) {
+            String input = reader.getInput();
+            input=input.trim(); //8. it removes the extra spaces from input
+            
+            if(input.startsWith("bye")) {
+                finished = true;
+            }
+            else {
+                String response = responder.generateResponse();
+                System.out.println(response);
+            }
+        }
+
+        printGoodbye();
+    }
+
+    /**
+     * Print a welcome message to the screen.
+     */
+    private void printWelcome()
+    {
+        System.out.println("Welcome to the DodgySoft Technical Support System.");
+        System.out.println();
+        System.out.println("Please tell us about your problem.");
+        System.out.println("We will assist you with any problem you might have.");
+        System.out.println("Please type 'bye' to exit our system.");
+    }
+
+    /**
+     * Print a good-bye message to the screen.
+     */
+    private void printGoodbye()
+    {
+        System.out.println("Nice talking to you. Bye...");
+    }
+}
